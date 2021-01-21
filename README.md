45e7fd399cf63d0b744b5ba761a037eb5cfc2532
xuwenjie <xuwenjie@travelsky.com>
2021-01-21 21:16:48 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # springcloud-demo
 springcloud微服务框架搭建demo
 
+Ribbon用于负载均衡，默认策略是轮询
+
+Feign集成了Ribbon，使得拥有了负载均衡的能力，默认策略是轮询(当存在服务降级时，轮询可能失效)
+
+Hystrix用于避免雪崩问题，拥有线程隔离，服务降级和服务熔断
+
 注：Eureka2.0非开源，Consul中国区锁定，注册中心建议Nacos
--- consumer-ribbon/src/main/java/com/xwj/springcloud/consumerribbon/controller/DemoController.java
+++ consumer-ribbon/src/main/java/com/xwj/springcloud/consumerribbon/controller/DemoController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Description TODO
+ * @Description 注意服务降级和熔断区别，降级是超过多久返回错误，熔断是
+ * 连续超时10次以后断开，直接返回错误，5秒后记录成功百分比一次断定是否结束熔断
  * @Author yuki
  * @Date 2019/3/27 11:08
  * @Version 1.0
@@ -21,12 +22,11 @@ public class DemoController {
     @Autowired
     DemoService demoService;
     @RequestMapping("/hi")
-//    单独设置降级时间，注意服务降级和熔断区别，降级是超过多久返回错误，熔断是
-//    连续超时10次以后断开，直接返回错误，5秒后记录成功百分比一次断定是否结束熔断
+//    单独设置降级时间
 //    @HystrixCommand(commandProperties = {
 //            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
 //    })
-//    单独设置方法的降级返回
+//    单独设置该方法的降级返回
 //    @HystrixCommand(fallbackMethod = "hiError")
 //    熔断修改默认设置
 //    @HystrixCommand(commandProperties = {
@@ -38,9 +38,11 @@ public class DemoController {
     public String hi(@RequestParam String name){
         return demoService.demoService(name);
     }
+//    单独方法的降级返回
 //    public String hiError(String name){
 //        return "服务器超时";
 //    }
+    // 该类的降级返回
     public String defaultError(){
         return "服务器超时";
     }
